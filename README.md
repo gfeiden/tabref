d74c04127d891711fc017891584c91ba00ebba5e
gfeiden <gregory.a.feiden@gmail.com>
2015-07-02 15:28:57 +0200

    Updated readme and cleaned script header

--- README.md
+++ README.md
@@ -1,3 +1,48 @@
 # tabref.py
 
+Numerically order table reference with ease.
 
+## Purpose
+The purpose of the routines listed below is to extract and 
+sequentially number, in order of appearance, citations from a LaTeX 
+table that uses the deluxetable environment from AASTeX.
+          
+## Dependencies
+Functions contained below rely only on the standard Python libraries
+'sys' and 'fileinput'.
+                      
+## Notes
+The file input into the module must contain a LaTeX table formatted 
+using the deluxetable environment from the AASTeX or emulateapj 
+packages.
+    
+## Examples
+Before demonstrating how to call the script, you'll want to know what
+you'll need for input and what to expect as output.
+
+### Input
+ * f : String of the file name containing a LaTeX table.
+
+ * cols : (Optional) List or tuple whose elements are the column number
+          for the citation columns.
+
+### Output
+ * f_new : Reconstructed LaTeX file containing updated table with the
+           file name: <filename>.tex_new
+
+The function may be called from the command line:
+
+```shell    
+python tabref.py ./dir/<filename>.tex col1 col2 ...
+```       
+                 
+Alternatively, the function may be called from within a python shell:
+
+```python    
+import tabref
+tabref.order_refs('./dir/<filename>' [, cols=(col1,col2)])
+```
+
+where the 'cols' argument is optional only if the last column in the 
+table contains citations.
+                
--- tabref.py
+++ tabref.py
@@ -1,64 +1,13 @@
-"""
-    Beer-Ware Licence (Revision 42)
-    -------------------------------
-    Gregory A. Feiden wrote this file. As long as you retain this notice 
-    you can do whatever you want with this stuff. If we meet some day and 
-    you think this stuff is worth it, you can buy me a beer in return.
-    
-    Purpose
-    -------
-    The purpose of the routines listed below is to extract and 
-    sequentially number, in order of appearance, citations from a LaTeX 
-    table that uses the deluxetable environment from AASTeX.
-          
-    Input
-    -----
-    f : String of the file name containing a LaTeX table.
-    
-    cols : (Optional) List or tuple whose elements are the column number 
-           for the citation columns.
-   
-    Output
-    ------
-    f_new : Reconstructed LaTeX file containing updated table with the
-            file name: <filename>.tex_new
-            
-    Dependencies
-    ------------
-    Functions contained below rely only on the standard Python libraries
-    'sys' and 'fileinput'.
-                      
-    Notes
-    ------
-    The file input into the module must contain a LaTeX table formatted 
-    using the deluxetable environment from the AASTeX or emulateapj 
-    packages.
-    
-    Examples
-    --------
-    The function may be called from the command line:
-    
-                $ python tabref.py ./dir/<filename>.tex col1 col2 ...
-                        
-    Alternatively, the function may be called from within a python shell:
-    
-                >>> import tabref
-                >>> tabref.order_refs('./dir/<filename>' [, cols=(col1,col2)])
-    
-    where the 'cols' argument is optional only if the last column in the 
-    table contains citations.
-                
-    Author
-    ------
-    Gregory A. Feiden  <gregory.a.feiden.gr@dartmouth.edu>
-    
-    Date
-    ----
-    12 Nov 2012
-  
-"""
-__all__ = [ 'check_files', 'create_ref_dict', 'order_refs', 'tablenotegen',
-            'write_to_file' ]
+#
+#
+
+__all__ = [ "check_files", "create_ref_dict", "order_refs", "tablenotegen",
+            "write_to_file" ]
+
+__version__ = "0.1.0"
+__author__ = "Gregory Feiden <gregory.a.feiden@gmail.com>"
+__copyright__ = "Copyright 2015 Gregory A. Feiden \n Licensed under Beerware (Revision 42)"
+__contributors__ = []
             
 import sys
 import fileinput as _fi
